e92fa8daee1d2e05d93463a3bb9e77dae522d151
Marcos <terceiro.marcos2005@gmail.com>
2025-08-21 15:42:59 -0300

    ajustando models

--- backend/src/models/anexo.js
+++ backend/src/models/anexo.js
@@ -2,38 +2,18 @@ const mongoose = require("mongoose");
 
 const anexoSchema = new mongoose.Schema(
   {
-    // Caminho local no servidor
-    url: {
-      type: String, // ex: "uploads/nome_arquivo.jpg"
-      required: true,
-    },
-
-    // metadados do arquivo
-    tipo: {
-      type: String,
-      enum: ["imagem", "video"],
-      required: true,
-    },
-    nomeOriginal: {
-      type: String,
-      required: true,
-    },
-    tamanho: {
-      type: Number,
-      required: true,
-    },
-    formato: {
-      type: String, // ex: "image/jpeg", "video/mp4"
-    },
+    url: { type: String, required: true },
+    tipo: { type: String, enum: ["imagem", "video"], required: true },
+    nomeOriginal: { type: String, required: true },
+    tamanho: { type: Number, required: true },
+    formato: { type: String },
     metadata: {
-      largura: { type: Number }, // somente para imagens
-      altura: { type: Number },  // somente para imagens
-      duracao: { type: Number }, // somente para vídeos
+      largura: { type: Number },
+      altura: { type: Number },
+      duracao: { type: Number },
     },
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
-module.exports = mongoose.model("Anexo", anexoSchema);
+module.exports = anexoSchema;
